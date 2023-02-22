9571b08acc7e0fba5c21f1a34476e421aa00c517
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-02-22 19:19:33 +0545

    files

---
+++ agrolink/src/assets/data/serviceData.js
@@ -0,0 +1,28 @@
+const serviceData = [
+  {
+    icon: "ri-service-line",
+    title: "Variety of Product",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#fdefe6",
+  },
+  {
+    icon: "ri-shopping-bag-line",
+    title: "Easy Buying Process",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#ceebe9",
+  },
+  {
+    icon: "ri-secure-payment-line",
+    title: "Secure Payment",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#e2f2b2",
+  },
+  {
+    icon: "ri-truck-line",
+    title: "Easy Shipping",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#d6e5fb",
+  },
+];
+
+export default serviceData;
